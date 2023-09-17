973be87867f0ac981682893453704265d04b8511
waelbessaies <waelbessaies2@gmail.com>
2023-09-17 20:40:22 +0100

    Execution stack & timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,21 @@
+function countPrimeNumbers() {
+    var primeNumbers = 0;
+    for (var i = 2; i <= 100; i++) {
+        let prime = true;
+        for (var j = 2; j < i; j++) {
+            if (i % j === 0) {
+                prime = false;
+            }
+        }
+        if (prime) primeNumbers++;
+    }
+    return primeNumbers;
+}
+
+const Triggertime = performance.now();
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+}
+const Terminationtime = performance.now();
+const executionTime = Terminationtime - Triggertime;
+console.log(`Execution time of calculating prime numbers 100 times was ${executionTime} milliseconds.`);
